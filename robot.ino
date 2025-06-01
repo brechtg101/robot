ae0311fbfe69014216020ec8b5746894bc5c6012
Koen Gullentops <koen.gullentops@etexgroup.com>
2025-06-01 17:08:45 +0200

    Initial commit: Adding robot source code with line following and obstacle avoidance functionality

---
+++ robot.ino
@@ -0,0 +1,222 @@
+/*
+ * Lijnvolgende robot met obstakelvermijding
+ * Origineel door ASHIQ SHIBU (MAKER Q)
+ * Nederlandse versie
+ */
+
+// ===== CONFIGURATIE VARIABELEN =====
+// Snelheidsinstellingen voor de motoren
+int vSpeed = 80;         // Normale rijsnelheid
+int turn_speed = 160;    // Snelheid tijdens draaien (0-255)
+int t_p_speed = 80;      // Snelheid tijdens obstakelvermijding
+int stop_distance = 12;  // Afstand in cm waarop robot stopt voor obstakel
+int turn_delay = 10;     // Vertraging tijdens draaien
+
+// ===== PIN CONFIGURATIE =====
+// Ultrasone sensor (HC-SR04) aansluitingen
+const int trigPin = 25;  // Trigger pin
+const int echoPin = 26;  // Echo pin
+
+// Motor driver (L293) aansluitingen
+const int motorR1 = 22;      // Rechter motor richting pin 1
+const int motorR2 = 23;      // Rechter motor richting pin 2
+const int motorRspeed = 15;  // Rechter motor snelheid pin
+const int motorL1 = 2;       // Linker motor richting pin 1
+const int motorL2 = 14;      // Linker motor richting pin 2
+const int motorLspeed = 16;  // Linker motor snelheid pin
+
+// Lijnsensor aansluitingen
+const int left_sensor_pin = 33;   // Linker lijnsensor
+const int right_sensor_pin = 35;  // Rechter lijnsensor
+
+// ===== GLOBALE VARIABELEN =====
+int turnspeed;            // Variabele voor draaisnelheid
+int left_sensor_state;    // Status linker lijnsensor
+int right_sensor_state;   // Status rechter lijnsensor
+long duration;            // Tijdsduur voor ultrasone sensor
+int distance;            // Gemeten afstand in cm
+
+// ===== MOTOR FUNCTIES =====
+void stopMotors() {
+  analogWrite(motorRspeed, 0);
+  analogWrite(motorLspeed, 0);
+}
+
+void driveForward(int speed) {
+  digitalWrite(motorR1, HIGH);
+  digitalWrite(motorR2, LOW);                       
+  digitalWrite(motorL1, HIGH);
+  digitalWrite(motorL2, LOW);
+  analogWrite(motorRspeed, speed);
+  analogWrite(motorLspeed, speed);
+}
+
+void turnRight(int speed) {
+  digitalWrite(motorR1, LOW);
+  digitalWrite(motorR2, HIGH);                       
+  digitalWrite(motorL1, HIGH);
+  digitalWrite(motorL2, LOW);
+  analogWrite(motorRspeed, speed);
+  analogWrite(motorLspeed, turn_speed);
+}
+
+void turnLeft(int speed) {
+  digitalWrite(motorR1, HIGH);
+  digitalWrite(motorR2, LOW);                       
+  digitalWrite(motorL1, LOW);
+  digitalWrite(motorL2, HIGH);
+  analogWrite(motorRspeed, turn_speed);
+  analogWrite(motorLspeed, speed);
+}
+
+void turnRightSharp(int speed) {
+  digitalWrite(motorR1, LOW);
+  digitalWrite(motorR2, HIGH);                       
+  digitalWrite(motorL1, LOW);
+  digitalWrite(motorL2, HIGH);
+  analogWrite(motorRspeed, speed);
+  analogWrite(motorLspeed, speed);
+}
+
+void turnLeftSharp(int speed) {
+  digitalWrite(motorR1, HIGH);
+  digitalWrite(motorR2, LOW);                       
+  digitalWrite(motorL1, HIGH);
+  digitalWrite(motorL2, LOW);
+  analogWrite(motorRspeed, speed);
+  analogWrite(motorLspeed, speed);
+}
+
+// ===== OBSTAKELVERMIJDING FUNCTIE =====
+void avoidObstacle() {
+  Serial.println("Obstakel gedetecteerd! Start ontwijkmanoeuvre");
+  Serial.print("Afstand tot obstakel: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  // Ontwijkmanoeuvre naar links
+  turnLeftSharp(t_p_speed);
+  delay(250);
+  
+  // Stop
+  stopMotors();
+  delay(500);
+  
+  // Draai
+  turnLeftSharp(t_p_speed);
+  delay(900);
+
+  // Rij vooruit
+  driveForward(t_p_speed);
+  delay(800);
+
+  // Draai terug
+  turnRightSharp(t_p_speed);
+  delay(900);
+
+  // Rij vooruit
+  driveForward(t_p_speed);
+  delay(700);
+
+  // Draai
+  turnRightSharp(t_p_speed);
+  delay(650);
+
+  // Rij vooruit
+  driveForward(t_p_speed);
+
+  // Zoek naar lijn
+  left_sensor_state = HIGH;
+  while(left_sensor_state == LOW) {
+    left_sensor_state = digitalRead(left_sensor_pin);
+    right_sensor_state = digitalRead(right_sensor_pin);
+    Serial.println("Zoeken naar lijn...");
+    delay(100);
+  }
+
+  // Finale positionering
+  turnLeftSharp(t_p_speed);
+  delay(100);
+  
+  driveForward(t_p_speed);
+  delay(500);
+}
+
+void setup() {
+  // Start seriële communicatie
+  Serial.begin(115200);
+  Serial.println("\n=== Lijnvolgende Robot met Obstakelvermijding ===");
+  Serial.println("Initialiseren van pinnen...");
+  
+  // Configureer motor pinnen als output
+  pinMode(motorR1, OUTPUT);
+  pinMode(motorR2, OUTPUT);
+  pinMode(motorL1, OUTPUT);
+  pinMode(motorL2, OUTPUT);
+  Serial.println("Motor pinnen geïnitialiseerd");
+
+  // Configureer ultrasone sensor pinnen
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.println("Ultrasone sensor pinnen geïnitialiseerd");
+  
+  // Configureer lijnsensor pinnen
+  pinMode(left_sensor_pin, INPUT);
+  pinMode(right_sensor_pin, INPUT);
+  Serial.println("Lijnsensor pinnen geïnitialiseerd");
+
+  Serial.println("Setup voltooid. Starten over 3 seconden...");
+  delay(3000);                              
+  Serial.println("Robot is nu actief!");
+}
+
+void loop() {
+  // Lees ultrasone sensor
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Afstand: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  // Lees lijnsensoren
+  left_sensor_state = digitalRead(left_sensor_pin);
+  right_sensor_state = digitalRead(right_sensor_pin);
+  Serial.print("Lijnsensoren - Links: ");
+  Serial.print(left_sensor_state);
+  Serial.print(" Rechts: ");
+  Serial.println(right_sensor_state);
+
+  // Besturing op basis van lijnsensor input
+  if(right_sensor_state == LOW && left_sensor_state == HIGH) {
+    Serial.println("Actie: Draait naar rechts");
+    turnRight(vSpeed);
+  }
+  else if(right_sensor_state == HIGH && left_sensor_state == LOW) {
+    Serial.println("Actie: Draait naar links");
+    turnLeft(vSpeed);
+  }
+  else if(right_sensor_state == HIGH && left_sensor_state == HIGH) {
+    Serial.println("Actie: Rijdt vooruit");
+    driveForward(vSpeed);
+    delay(turn_delay);
+  }
+  else if(distance < stop_distance) {
+    avoidObstacle();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+ 
